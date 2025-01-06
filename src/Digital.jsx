1ae9e2bd49b9d39181e0b4bb8649df33a80e1dbc
Prince-Dawn <princedawnsylvanus@gmail.com>
2025-01-06 19:34:35 +0100

    responsive navbar

--- src/Digital.jsx
+++ src/Digital.jsx
@@ -9,7 +9,7 @@ export default function Digital() {
   return (
     <>
         <div className="  xxl:w-full xxl:h-[70vh] xxl:bg-cover xxl:bg-top xxl:mt-24 xxl:relative " style={style} >
-            <div className="xxl:w-[45rem] xxl:border xxl:border-[rgb(42,38,55)] text-white xxl:h-[20rem] py-7 px-6 space-y-6 rounded-xl bg-gradient-to-t from-[rgb(20,8,32)] to-[rgba(40,32,55,0.8)] xxl:absolute xxl:-bottom-[5rem] xxl:left-[10rem] ssm:static ssm:w-full ssm:h-fit ssm:bg-none">
+            <div className="xxl:w-[45rem] xxl:border xxl:border-[rgb(42,38,55)] text-white xxl:h-[20rem] py-7 px-6 space-y-6 rounded-xl bg-gradient-to-t from-[rgb(20,8,32)] to-[rgba(40,32,55,0.8)] xxl:absolute xxl:-bottom-[5rem] xxl:left-[10rem] ssm:static ssm:w-full ssm:h-fit">
                 <span className=" xxl:text-4xl font-bold ssm:text-2xl">
                     Pioneering Digital Excellence: <br />
                     Discover the Digimax <br className="xxl:static ssm:hidden" />
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,14 +1,47 @@
 // import React from 'react'
+import { Menu, X } from "lucide-react";
+
+
+
+
+// let ham = document.getElementById('ham');
+// let x = document.getElementById('x');
+// let menu = document.getElementById('menu');
+//     function handleClick(){
+//         console.log('hello')
+//         alert('hello');
+//     }
+
+//     x.addEventListener("click", function() {
+//         menu.style.display='none';
+//         x.style.display='none';
+//         ham.style.display='block';
+        
+// });
+
+// ham.addEventListener("click", function() {
+//     menu.style.display='block';
+//     ham.style.display='none';
+//     x.style.display='block';
+
+// console.log(menu.style.display)
+// });
 
 export default function Hero() {
   return (
     <>
-        <div className="xxl:w-full xxl:h-[100vh] pt-2 ssm:h-[50rem]">
-            <div className="xxl:flex xxl:justify-between items-center xxl:flex-row xxl:h-[10%] px-14 text-white border-b-[0.1px] border-purple-500 ssm:hidden">
-                <div className=" text-4xl w-fit-content ">
+        <div className="xxl:w-full xxl:h-[100vh] xxl:pt-2 ssm:h-[52rem]">
+            <div className="xxl:flex xxl:justify-between items-center xxl:flex-row xxl:h-[10%] xxl:px-14 text-white border-b-[0.1px] border-purple-500 ssm:text-center ssm:h-[6%] ssm:flex ssm:items-center ssm:justify-center ssm:fixed ssm:top-0 ssm:w-full bg-[rgb(10,0,24)]">
+                <div className=" xxl:text-4xl xxl:w-[20%] ssm:text-3xl ssm:flex ssm:justify-around ssm:items-center ssm:w-full ">
+                    <button type="submit" id="ham" className="xxl:hidden ssm:flex" >
+                        <Menu />
+                    </button>
                     Digimax
+                    <button type="submit" id="x" className="xxl:hidden ssm:flex">
+                        <X/>
+                    </button>
                 </div>
-                <div className=" flex space-x-4 items-center ">
+                <div className=" xxl:flex xxl:space-x-4 xxl:items-center ssm:hidden " id="menu">
                     <ul className="flex space-x-4 text-xl">
                         <li className="">Homepage</li>
                         <li className="">About us</li>
@@ -20,8 +53,8 @@ export default function Hero() {
                 </div>
             </div>
             <div className="xxl:w-full h-[100%] xxl:flex xxl:flex-row xxl:justify-center xxl:items-center ssm:flex-col">
-                <div className="xxl:h-full xxl:w-1/2 xxl:flex xxl:justify-center xxl:items-center xxl:flex-wrap ssm:w-full  ssm:justify-start ssm:items-start ssm:px-5">
-                    <div className=" space-y-7 ssm:h-[fit-content]">
+                <div className="xxl:h-full xxl:w-1/2 xxl:flex xxl:justify-center xxl:items-center xxl:flex-wrap ssm:w-full  ssm:justify-start ssm:items-start ssm:px-5 ssm:pt-[10%]">
+                    <div className=" space-y-7 ssm:h-[fit-content] py-5">
                         <span className="text-[rgb(255,140,193)] text-base ">WEBSITE DEVELOPMENT</span>
                         <div className=" ">
                             <span className=" text-white xxl:text-5xl font-bold ssm:text-xl">Delivering IT solutions that <br /> enable you to work smarter.</span>
